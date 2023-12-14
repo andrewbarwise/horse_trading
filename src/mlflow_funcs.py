7912f060bf789422b11388c3283f647ef97f594c
Andrew Barwise <andrew.barwise@gmail.com>
2023-12-14 20:21:50 +0000

    adding function to evaluate models

--- src/mlflow_funcs.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-
-def log_metrics_to_mlflow(accuracy, precision, recall, f1, roc_auc):
-    """Logs classification metrics to MLflow."""
-    mlflow.log_metric('accuracy', accuracy)
-    mlflow.log_metric('precision', precision)
-    mlflow.log_metric('recall', recall)
-    mlflow.log_metric('f1_score', f1)
-    mlflow.log_metric('roc_auc', roc_auc)

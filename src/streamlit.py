52f5c2acb2599a4c061273a6dc54c39c49751e59
Andrew Barwise <andrew.barwise@gmail.com>
2024-01-10 17:28:13 +0000

    changes to date_list()

--- src/streamlit.py
+++ src/streamlit.py
@@ -3,7 +3,17 @@ from datetime import datetime, timedelta
 
 def date_list():
     tz = pytz.timezone('UTC')
-    date_list = [(datetime.today() + timedelta(days=i)) \
-                 .replace(tzinfo=tz).strftime('%Y-%m-%dT%H:%M:%SZ') for i in range(3)]
-    
-    return date_list
+    start_times = []
+    end_times = []
+
+    for ii in range(3):
+        start_time = datetime.utcnow() + timedelta(days = ii)
+        start_time = start_time.replace(hour=0, minute=0, second=0, microsecond=0)
+        start_time = tz.localize(start_time).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        end_time = start_time[:-1] + '23:59:59Z'
+
+        start_times.append(start_time)
+        end_times.append(end_time)
+
+    return start_times, end_times

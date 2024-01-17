d4df8a8c5f5ee30baad655cc6f9275e7104a9b2f
Andrew Barwise <andrew.barwise@gmail.com>
2024-01-17 19:43:27 +0000

    fix up the login page and clean up root directory

--- streamlit_login_page.py
+++ streamlit_login_page.py
@@ -1,31 +1,3 @@
-import streamlit_pages as st 
-from streamlit_pages import *
-from src.betfair import Betfair
+from src.streamlit_pages import SessionState, login, login_page
 
-api_key = 'ABGJLOlKaLtTsMIp'
-auth_token = 'SWahjOHWwjsxOEg+u232YS5eLrnFA8pDeM8J1LTAl70='
-
-# create instance of Betfair class
-bf = Betfair(api_key, auth_token)
-
-# check connection
-if st.button('Check Betfair connection'):
-    bf.check_connection()
-
-# display account balance
-balance = bf.account_balance()
-st.metric(label = 'Account Balance', value = balance)
-
-dates = date_list()
-
-
-# create a date dropdown box
-selected_date = st.selectbox('Select a date', date_list())
-
-start_time, end_time = format_selected_date(selected_date)
-
-# check if valid dates are returned
-if start_time and end_time:
-    bf.list_market_horse(start_time, end_time)
-else:
-    st.error('Invalid date selected')
+login_page()
